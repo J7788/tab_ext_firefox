1ff545bea5efbe94036c5a3753bb5bb51b859c00
J7788 <j.h.lunz@gmail.com>
2022-06-29 17:20:27 -0500

    Implement direct tabbing functionality

---
+++ tab_first.js
@@ -0,0 +1,13 @@
+
+let new_page = true;
+document.addEventListener('keydown', (event) => {
+    if(event.key == "Tab" && new_page){
+        let focusabeElements = document.body.querySelectorAll(
+            'a[href]:not([disabled])'
+            );
+        focusabeElements[26]?.focus();
+        console.log(focusabeElements)
+        event.preventDefault();
+        new_page = false;
+    }
+}, false)
